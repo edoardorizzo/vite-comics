84551d146126204e7e54e1e6807f56a87f84b47c
edoardorizzo <edo96.rizzo@gmail.com>
2023-04-13 15:57:33 +0200

    add footer

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,52 @@ export default {
     </div>
   </main>
 
-  <footer></footer>
+  <footer>
+    <div class="container">
+      <div class="left_footer">
+        <ul>
+          <h4>DC COMICS</h4>
+          <li>Characters</li>
+          <li>Comics</li>
+          <li>Movies</li>
+          <li>Tv</li>
+          <li>Games</li>
+          <li>Videos</li>
+          <li>News</li>
+        </ul>
+        <ul>
+          <h4>DC</h4>
+          <li>Term Of Use</li>
+          <li>Privacy Policy</li>
+          <li>Add Choice</li>
+          <li>Advertising</li>
+          <li>Jobs</li>
+          <li>Subscriptions</li>
+          <li>Talent</li>
+          <li>CPSC</li>
+          <li>Ratings</li>
+          <li>Shop Help</li>
+        </ul>
+        <ul>
+          <h4>SITES</h4>
+          <li>DC</li>
+          <li>Mad Magazine</li>
+          <li>DC Kids</li>
+          <li>Advertising</li>
+        </ul>
+        <ul>
+          <h4>SHOP DC</h4>
+          <li>Shop DC</li>
+          <li>Shop Collectibles</li>
+        </ul>
+      </div>
+      <div class="right_footer">
+        <div class="logo_footer_container">
+          <img src="/img/dc-logo.png" alt="">
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
@@ -81,5 +126,44 @@ main {
 
 main p {
   color: white;
+}
+
+/* main */
+
+/* footer */
+
+footer {
+  background-image: url(/img/footer-bg.jpg);
+  background-repeat: no-repeat;
+}
+
+.left_footer {
+  display: flex;
+  padding: 2rem 0;
+}
+
+footer ul {
+  margin: 0 1rem;
+}
+
+footer h4 {
+  color: white;
+}
+
+footer li {
+  color: grey;
+  font-size: 12px;
+  list-style: none;
+  margin: 0.25rem 0;
+}
+
+.logo_footer_container{
+  width: 300px;
+}
+
+.logo_footer_container img{
+  width: 100%;
+  display: block;
+  transform: rotate(-45deg);
 }
 </style>

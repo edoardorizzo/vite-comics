847fde29f60b1ce067fba63002735d4e4eca712a
edoardorizzo <edo96.rizzo@gmail.com>
2023-04-13 16:51:22 +0200

    add all site structure

--- src/App.vue
+++ src/App.vue
@@ -79,6 +79,33 @@ export default {
       </div>
     </div>
   </footer>
+
+  <div class="bottom_bar">
+    <div class="container">
+      <div class="bottom_bar_left">
+        <button>SIGN-UP NOW</button>
+      </div>
+      <div class="bottom_bar_right">
+        <span>FOLLOW US</span>
+        <div class="fb_icon icon">
+          <img src="/img/footer-facebook.png" alt="">
+        </div>
+        <div class="tw_icon icon">
+          <img src="/img/footer-twitter.png" alt="">
+        </div>
+        <div class="yt_icon icon">
+          <img src="/img/footer-youtube.png" alt="">
+        </div>
+        <div class="p_icon icon">
+          <img src="/img/footer-pinterest.png" alt="">
+        </div>
+        <div class="pin_icon icon">
+          <img src="/img/footer-periscope.png" alt="">
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -157,13 +184,53 @@ footer li {
   margin: 0.25rem 0;
 }
 
-.logo_footer_container{
+.logo_footer_container {
   width: 300px;
 }
 
-.logo_footer_container img{
+.logo_footer_container img {
   width: 100%;
   display: block;
   transform: rotate(-45deg);
+  filter: opacity(50%);
 }
+
+/* footer */
+
+/* bottom bar */
+
+.bottom_bar {
+  background-color: rgb(42, 42, 42);
+  padding: 1rem 0;
+}
+
+.bottom_bar button {
+  padding: 0.25rem;
+  border: 1px solid blue;
+  background-color: rgb(42, 42, 42);
+  color: white;
+}
+
+.bottom_bar_right{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.bottom_bar_right span{
+  color: blue;
+  font-size: 14px;
+  margin-right: 8px;
+}
+
+.icon{
+  width: 16px;
+  height: 16px;
+  margin: 0 0.25rem;
+}
+
+.icon img{
+  width: 100%;
+}
+
 </style>

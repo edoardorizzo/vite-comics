74cfadb3c6e1d67cee613c0dae5d69b05421f440
edoardorizzo <edo96.rizzo@gmail.com>
2023-04-14 11:45:55 +0200

    add SiteBanner component

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,24 @@ import SiteHeader from './components/SiteHeader.vue'
 import SiteMain from './components/SiteMain.vue'
 import SiteFooter from './components/SiteFooter.vue'
 import SiteBottomBar from './components/SiteBottomBar.vue'
+import SiteBanner from './components/SiteBanner.vue'
 export default {
   components: {
     SiteHeader,
     SiteMain,
     SiteFooter,
     SiteBottomBar,
+    SiteBanner,
   }
 }
 </script>
 
 <template>
-
   <SiteHeader></SiteHeader>
 
   <SiteMain></SiteMain>
+
+  <SiteBanner></SiteBanner>
 
   <SiteFooter></SiteFooter>
 
@@ -27,5 +30,6 @@ export default {
 
 <style lang="scss" scoped>
 @use './assets/partials/variables' as *;
+
 
 </style>
---
+++ src/components/SiteBanner.vue
@@ -0,0 +1,81 @@
+<script>
+    export default {
+        name: 'SiteBanner',
+        data (){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="banner">
+    <div class="banner_section">
+      <div class="image_container">
+        <img src="/img/buy-comics-digital-comics.png" alt="">
+      </div>
+      <span>Lorem Ipsum</span>
+    </div>
+    <div class="banner_section">
+      <div class="image_container">
+        <img src="/img/buy-comics-merchandise.png" alt="">
+      </div>
+      <span>Lorem Ipsum</span>
+    </div>
+    <div class="banner_section">
+      <div class="image_container">
+        <img src="/img/buy-comics-shop-locator.png" alt="">
+      </div>
+      <span>Lorem Ipsum</span>
+    </div>
+    <div class="banner_section">
+      <div class="image_container">
+        <img src="/img/buy-comics-subscriptions.png" alt="">
+      </div>
+      <span>Lorem Ipsum</span>
+    </div>
+    <div class="banner_section">
+      <div class="image_container">
+        <img src="/img/buy-dc-power-visa.svg" alt="">
+      </div>
+      <span>Lorem Ipsum</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/partials/variables' as *;
+
+.banner {
+  height: 80px;
+  background-color: $blue;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  .banner_section{
+    display: flex;
+    align-items: center;
+  }
+
+
+  .image_container {
+    height: 32px;
+    width: 24px;
+    display: flex;
+    margin: 0.5rem;
+  }
+
+  img {
+    width: 100%;
+  }
+
+  span {
+    color: $white;
+    font-size: 12px;
+    display: inline-block;
+  }
+}
+
+</style>
